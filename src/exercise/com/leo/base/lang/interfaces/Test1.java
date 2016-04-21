1460c972d0cef43f43817804468bcc6f12275b7b
icella <changshanzi@gmail.com>
2016-04-21 11:49:13 +0800

    print system evn property

--- src/exercise/com/leo/base/lang/interfaces/Test1.java
+++ src/exercise/com/leo/base/lang/interfaces/Test1.java
@@ -1,10 +1,14 @@
 package exercise.com.leo.base.lang.interfaces;
 
+import java.util.Map.Entry;
+
 public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		for (Entry<Object, Object> entry : System.getProperties().entrySet()) {
+			System.out.println(entry);
+		}
 	}
 
 }
